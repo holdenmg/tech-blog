16f22888e1e767fc722a92b82ceb07aac3ab2b72
Holden Garrison <holdengarrison@holdens-mbp-2.lan>
2023-08-10 21:26:52 -0400

    Wrote all models and updated relationships

--- models/index.js
+++ models/index.js
@@ -2,12 +2,21 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
-User.hasMany(Project, {
+User.hasMany(Post, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
